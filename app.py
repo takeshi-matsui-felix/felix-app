4ccb6d92763a5e9c5d7051f219ae589fa458fe14
松井武史_フィリックス <takeshi_matsui@felix-japan.co.jp>
2026-08-12 11:46:54 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1747,17 +1747,12 @@ def main():
                         <p style="font-size:14px; color:#333;">この検査記録の保存（右上の「Print」等）が完了しましたら、システム容量を空けるためにデータを削除してください。<br><b>※一度削除した写真は元に戻せません。</b></p>
                     </div>""", unsafe_allow_html=True)
                     del_pass = st.text_input("削除用パスワードを入力 (5963)", type="password", key=f"del_pass_all")
-                   if st.button(f"この検査（{type_val}）のデータを完全に削除する", key=f"del_btn_all"):
+                  if st.button(f"この検査（{type_val}）のデータを完全に削除する", key=f"del_btn_all"):
                         if del_pass == DELETE_PASSWORD:
                             with st.spinner("データ削除中..."):
                                 for iid in t_ids:
                                     requests.delete(f"{SUPABASE_URL}/rest/v1/inspection_records?inspection_id=eq.{iid}", headers=HEADERS)
                                     requests.delete(f"{SUPABASE_URL}/rest/v1/inspections?inspection_id=eq.{iid}", headers=HEADERS)
-                                
-                                # 🌟 以下の2行を追加！アプリの記憶（亡霊）を完全に消し去ります
-                                clear_specific_cache("inspection_records")
-                                clear_specific_cache("inspections")
-                                
                             st.success("すべてのデータの削除が完了しました"); st.session_state.drill_target = None; st.session_state.cached_records = None; time.sleep(1); st.rerun()
                         else: st.error("パスワードが違います")
                     st.markdown("<hr class='admin-delete-box'>", unsafe_allow_html=True)
